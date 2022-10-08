2afa047705faa0464b03dead26bab245ced00cc2
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2022-10-08 09:39:24 -0500

    Archivo7
    Lección 7: Herencia de entidades

--- Facturacion/src/com/tuempresa/facturacion/modelo/Categoria.java
+++ Facturacion/src/com/tuempresa/facturacion/modelo/Categoria.java
@@ -1,16 +1,10 @@
 package com.tuempresa.facturacion.modelo;
 	import javax.persistence.*;
-	import org.hibernate.annotations.GenericGenerator;
-	import org.openxava.annotations.*;
-	import lombok.*;
+
+import lombok.*;
 	@Entity @Getter @Setter
-public class Categoria {
-		@Id
-		@Hidden // La propiedad no se muestra al usuario. Es un identificador interno
-		@GeneratedValue(generator="system-uuid") // Identificador Universal Único (1)
-		@GenericGenerator(name="system-uuid", strategy = "uuid")
-		@Column(length=32)
-		String oid;
+public class Categoria extends Identificable{
+		
 		@Column(length=50)
 		String descripcion;
 }
